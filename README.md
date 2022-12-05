0951a809c98b0177147b7fdbb8368b5a9b2cec3e
Tyler-ytr <601576661@qq.com>
2022-12-06 00:12:36 +0800

    开机自启脚本注释

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # worker_notifier
+
+## 开机自启配置
+
+1. 搜索栏打开运行
+2. 输入`shell:startup`
+3. 把test.bat复制进去即可
+
+PS: 需要保证改脚本能正常运作；在尝试配置自启之前需要配置test.bat的路径，可以在bat末尾加上Pause来调试是否能够正确启动；
--- test.bat
+++ test.bat
@@ -1,6 +1,11 @@
+@REM ---用于隐藏cmd黑框---
 if "%1"=="hide" goto CmdBegin
 start mshta vbscript:createobject("wscript.shell").run("""%~0"" hide",0)(window.close)&&exit
 :CmdBegin
+@REM ---用于隐藏cmd黑框---
 D:
 cd \worker\help_worker
 python help_worker.py
+@REM ---用于调试闪退的情况---
+@REM Pause
+@REM ---用于调试闪退的情况---
